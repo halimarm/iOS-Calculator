c1c760c37bdabbdd7ef39530b6b8c98c0f657423
Halim Arumman <halimarumman@gmail.com>
2022-06-18 16:20:32 +0700

    Initial Commit

---
+++ Shared/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Shared
+//
+//  Created by Halim Arumman on 18/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
